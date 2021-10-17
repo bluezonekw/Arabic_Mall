364c584d9cfe899a6d7fef6af30d4e673b483d2e
MostafaMahmoud <bluezone.adv@gmail.com>
2021-10-17 13:38:53 +0200

    q

---
+++ Assets/scripts/gateController.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class gateController : MonoBehaviour
+{
+    public GameObject enter, exit;
+    public string shopIcon;
+    public GameObject welcome,header;
+    IEnumerator GetTextureRaw(string url, RawImage rawImage)
+    {
+        Resources.UnloadUnusedAssets();
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+            if (rawImage.texture)
+            {
+                Destroy(rawImage.texture);
+            }
+
+            rawImage.texture = DownloadHandlerTexture.GetContent(www);
+            shoWelcome();
+        }
+
+       
+    }
+    void showWelcome()
+    {
+        welcome.GetComponent<ArabicText>().Text = " يرحب بك! ";
+        StartCoroutine(GetTextureRaw(shopIcon, welcome.transform.parent.GetChild(0).gameObject.GetComponent<RawImage>()));
+        
+    }
+    void shoWelcome()
+    {
+        welcome.transform.parent.gameObject.SetActive(true);
+        Invoke("hideWelcome", 2f);
+    }
+    void hideWelcome()
+    {
+        welcome.transform.parent.gameObject.SetActive(false);
+
+    }
+    bool finished = true;
+    string token = "";
+    public void gate(GameObject Gate)
+    {
+        if (Gate.name == "enter")
+        {
+            enter.SetActive(false);
+            exit.SetActive(true);
+            PlayerPrefs.SetInt("EnterShopNumber", int.Parse(this.gameObject.name));
+            showWelcome();
+        }
+        else
+        {
+          
+
+            showCart(hostManager.domain + "api/cart/");
+        }
+       
+    }
+    public GameObject exitPopUp;
+    public void Exit()
+    {
+        enter.SetActive(true);
+        exit.SetActive(false);
+        PlayerPrefs.SetInt("EnterShopNumber", 0);
+    }
+    IEnumerator getShopData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(hostManager.domain + "api/stores/" + this.gameObject.name);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            getShopData();
+            //popup server error
+            Debug.Log(www.error);
+        }
+        else
+        {
+           
+            Response response = JsonUtility.FromJson<Response>(www.downloadHandler.text);
+
+           
+
+        }
+    }
+    public void generateToken()
+    {
+        finished = false;
+
+        Firebase.Auth.FirebaseAuth auth;
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+
+        user.TokenAsync(true).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("TokenAsync was canceled.");
+                return;
+            }
+
+            if (task.IsFaulted)
+            {
+                Debug.LogError("TokenAsync encountered an error: " + task.Exception);
+                return;
+            }
+            finished = true;
+            string idToken = task.Result;
+            token = idToken;
+
+
+
+        });
+
+    }
+    // public ThirdPersonUserInput joystick;
+    public void showCart(string url)
+    {
+        StartCoroutine(getCartItems(url));
+    }
+
+    IEnumerator getCartItems(string url)
+    {
+        while (!finished)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+        }
+        if (token != "")
+        {
+            PlayerPrefs.SetString("MallTokenId", token);
+        }
+        using (UnityWebRequest www = UnityWebRequest.Get(hostManager.domain + "api/cart/"))
+        {
+           
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.SetRequestHeader("Authorization", PlayerPrefs.GetString("MallTokenId"));
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                generateToken();
+                showCart(hostManager.domain + "api/cart/");
+                Debug.Log(www.error);
+            }
+            else
+
+            {
+                Response2 res = JsonUtility.FromJson<Response2>(www.downloadHandler.text);
+                if (res.results.Length > 0)
+                {
+               //     joystick.stop();
+                    header.SetActive(false);
+                    exitPopUp.SetActive(true);
+                    exitPopUp.GetComponent<exitPopupController>().shop = this.gameObject;
+                }
+                else
+                {
+                    Exit();
+                }
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+    [System.Serializable]
+    public class Response2
+    {
+        public int id;
+        public string next;
+        public string previous;
+        public result[] results;
+    }
+
+    [System.Serializable]
+    public class result
+    {
+        public int id;
+        public int quantity;
+        public int max_quantity;
+        public int item_unit;
+        public string color;
+        public Item item;
+    }
+
+    [System.Serializable]
+    public class Item
+    {
+        public int id;
+        public Images[] images;
+        public UnitData[] units;
+        public string name;
+        public string description;
+        public string price;
+
+    }
+    [System.Serializable]
+    public class Images
+    {
+
+        public int id;
+        public string file;
+    }
+
+
+    [System.Serializable]
+    public struct UnitData
+    {
+
+        public int id;
+        public string color;
+        public string unit;
+        public int quantity;
+    }
+}
---
+++ Assets/scripts/itemController.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class itemController : MonoBehaviour
+{
+    public GameObject content;
+    public GameObject back;
+
+    public GameObject standItem;
+    public GameObject loading;
+
+    string token = "";
+    bool finished = true;
+    public void generateToken()
+    {
+        finished = false;
+
+        Firebase.Auth.FirebaseAuth auth;
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+
+        user.TokenAsync(true).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("TokenAsync was canceled.");
+                return;
+            }
+
+            if (task.IsFaulted)
+            {
+                Debug.LogError("TokenAsync encountered an error: " + task.Exception);
+                return;
+            }
+            finished = true;
+            string idToken = task.Result;
+            token = idToken;
+
+
+
+        });
+
+    }
+    private void OnEnable()
+    {
+       // loading.SetActive(true);
+       
+        
+    }
+    string standID;
+    public void showItems(string standNumber)
+    {
+        standID = standNumber;
+        PlayerPrefs.SetString("EnterStandNumber", standNumber);
+        PlayerPrefs.SetString("EnterStandNumber", standNumber);
+        StartCoroutine(getItems(standNumber));
+            }
+
+    IEnumerator getItems(string standNumber)
+    {
+        while (!finished)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+        }
+        if (token != "")
+        {
+            PlayerPrefs.SetString("MallTokenId", token);
+        }
+        using (UnityWebRequest www = UnityWebRequest.Get(hostManager.domain + "api/stores/"+ PlayerPrefs.GetInt("EnterShopNumber")+"/stands/"+ standNumber))
+        {
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.SetRequestHeader("Authorization", PlayerPrefs.GetString("MallTokenId"));
+            yield return www.SendWebRequest();
+           // loading.SetActive(false);
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                generateToken();
+                StartCoroutine(getItems(standID));
+                Debug.Log(www.error);
+            }
+            else
+
+            {
+                Debug.Log(www.downloadHandler.text);
+                Parse(www.downloadHandler.text);
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+    public GameObject noItems;
+    void Parse(string response)
+    {
+        noItems.SetActive(false);
+        for (int i = content.transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(content.transform.GetChild(i).gameObject);
+        }
+        Response res= JsonUtility.FromJson<Response>(response);
+        if (res.items.Length == 0)
+        {
+            noItems.SetActive(true);
+
+        }
+        for (int i = 0; i < res.items.Length; i++)
+        {
+            GameObject item = Instantiate(standItem);
+            item.transform.parent = content.transform;
+            item.GetComponent<itemManager>().createItem(res.items[i].is_favourite, res.items[i].name, res.items[i].description, res.items[i].price, res.items[i].images[0].file, res.items[i].id.ToString());
+
+        }
+
+        back.SetActive(true);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    [System.Serializable]
+    public class Response
+    {
+        public int id;
+        public item[] items;
+    }
+
+    [System.Serializable]
+    public class item
+    {
+        public int id;
+        public bool is_favourite;
+        public string name;
+        public string description;
+        public string price;
+        public Images[] images;
+
+    }
+    [System.Serializable]
+    public class Images
+    {
+
+        public int id;
+        public string file;
+    }
+    
+}
