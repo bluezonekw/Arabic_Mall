a3e442428406998833b64c04bd27989ce9ac4c88
MostafaMahmoud <bluezone.adv@gmail.com>
2021-10-17 12:35:29 +0200

    Q
    Q

---
+++ Assets/InAppBrowser/Example/BrowserOpener.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrowserOpener : MonoBehaviour
+{
+    public GameObject mall;
+    public GameObject favcartCounter;
+    public string pageToOpen = "https://www.google.com";
+
+    bool IsInAppBrowserOpened = true;
+    // check readme file to find out how to change title, colors etc.
+    public void reopenBrowser(string url) {
+        pageToOpen = url;
+
+      
+        InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+        options.displayURLAsPageTitle = false;
+        options.hidesTopBar = false;
+       
+        options.backButtonText = "رجوع";
+        InAppBrowser.OpenURL(pageToOpen, options);
+        IsInAppBrowserOpened = true;
+    }
+
+	public void OnClearCacheClicked() {
+		InAppBrowser.ClearCache();
+    }
+    public void close()
+    {
+        mall.SetActive(true);
+        IsInAppBrowserOpened = false;
+        favcartCounter.SetActive(false);
+        favcartCounter.SetActive(true);
+        print("close browser");
+        InAppBrowser.CloseBrowser();
+    }
+}
---
+++ Assets/genderController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class genderController : MonoBehaviour
+{
+    public Actions action;
+    public GameObject male, female;
+    private void OnEnable()
+    {
+        if (!PlayerPrefs.HasKey("MallGender"))
+        {
+            PlayerPrefs.SetInt("MallGender", 0);
+        }
+        if (PlayerPrefs.GetInt("MallGender") == 0)
+        {
+            male.SetActive(true);
+            female.SetActive(false);
+
+        }
+        else if (PlayerPrefs.GetInt("MallGender") == 1)
+        {
+
+            male.SetActive(false);
+            female.SetActive(true);
+        }
+    }
+    public void changeGender(int index)
+    {
+        print(index);
+        if (index == 0)
+        {
+            male.SetActive(true); 
+            female.SetActive(false);
+
+        }
+        else if (index == 1)
+        {
+            male.SetActive(false);
+            female.SetActive(true);
+        }
+        PlayerPrefs.SetInt("MallGender", index);
+        if (action != null)
+        {
+            action.changeGender();
+        }
+    }
+}
