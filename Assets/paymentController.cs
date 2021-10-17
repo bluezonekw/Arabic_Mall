250ab785a48e7b45b77079f4f23d9d6f8142b650
MostafaMahmoud <bluezone.adv@gmail.com>
2021-10-17 13:35:17 +0200

    Q

---
+++ Assets/paymentController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paymentController : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void proceed(string paymentMethodId)
+    {
+
+    }
+}
